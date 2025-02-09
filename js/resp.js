299657f404afb3f332324b8c972465d5f513b644
arzoo0511 <148741922+arzoo0511@users.noreply.github.com>
2025-02-09 12:21:39 +0530

    Create resp.js

---
+++ js/resp.js
@@ -0,0 +1,12 @@
+burger=document.querySelector('.burger')
+navbar=document.querySelector('.navbar')
+leftnav=document.querySelector('.leftnav')
+rightnav=document.querySelector('.rightnav')
+
+
+burger.addEventListener('click',()=>{
+    rightnav.classList.toggle('v-class-resp');
+    navbar.classList.toggle('h-class-resp');
+    leftnav.classList.toggle('v-class-resp');
+
+})
